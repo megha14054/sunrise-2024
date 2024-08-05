6e16921c160d4a06873adc9f9a9dc07b52edfa5a
Megha100504 <meghak0510@gmail.com>
2024-08-06 04:04:45 +0530

    Task bar

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Sidebar from '@/components/Sidebar/Sidebar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="app-container">
+      {/* <Sidebar /> */}
+      <div className="main-content">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
